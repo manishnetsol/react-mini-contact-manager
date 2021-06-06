d21be7ba86147961083bae6d289ed985ecb1813a
manishnetsol <manish.sharma@netsolutions.com>
2021-06-06 20:59:44 +0530

    first commit with whole contact card mini project

---
+++ contact-app/src/components/ContactList.js
@@ -0,0 +1,56 @@
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+  const inputE1 = useRef("");
+
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
+
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHandler={deleteContactHandler}
+        key={contact.id}
+      />
+    );
+  });
+
+  const getSearchTerm = () => {
+    props.searchKeyword(inputE1.current.value);
+  };
+  return (
+    <div className="main">
+      <h2>
+        Contact List
+        <Link to="/add">
+          <button className="ui button blue right">Add a Contact</button>
+        </Link>
+      </h2>
+
+      <div className="ui search">
+        <div className="ui icon input">
+          <input
+            ref={inputE1}
+            type="text"
+            placeholder="search contact"
+            className="prompt"
+            value={props.term}
+            onChange={getSearchTerm}
+          />
+          <i className="search icon"></i>
+        </div>
+      </div>
+      <div className="ui celled list">
+        {renderContactList.length > 0
+          ? renderContactList
+          : "No contacts Available"}
+      </div>
+    </div>
+  );
+};
+
+export default ContactList;
